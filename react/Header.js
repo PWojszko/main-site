60ae46b38078055bac304442b976a99f5d076195
PWojszko <piotr.wojszko@gmail.com>
2022-01-27 23:34:05 +0100

    Style changes

--- react/Header.js
+++ react/Header.js
@@ -1,21 +1,16 @@
 const Header = (props) => {
   return (
     <header className="header" id="header">
-      <GoToBottom />
-      <div className="header__wheel">
-        <div className="wheel__dot"></div>
-        <div className="wheel__dot"></div>
-        <div className="wheel__dot"></div>
-        <div className="wheel__dot"></div>
-        <div className="wheel__dot"></div>
-        <div className="wheel__dot"></div>
-        <div className="wheel__dot"></div>
-        <div className="wheel__dot"></div>
-      </div>
       <div className="header__text">
-        <h1>Lorem</h1>
-        <h2>Lorem</h2>
+        <h1>
+          <strong>Lorem</strong> ipsum
+        </h1>
+        <h2>
+          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ad quia
+          officia inventore similique minima.
+        </h2>
       </div>
+      <GoToBottom />
     </header>
   );
 };
